3bfa71a4face1474381854a58ca73b9e6a669486
celdyner <h.oranekwulu@gmail.com>
2022-10-10 20:19:11 +0100

    added routes

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require('express');
-
+const connectDB = require('./config/db');
+// const routes = require('./routes/api/auth');
+// const routes = require('./routes/api/posts');
+// const routes = require('./routes/api/profiles');
+// const routes = require('./routes/api/users');
 
 const app = express();
 
-//To the connection
-app.get('/', (req, res) => res.send('API Running'))
+//Connect Database
+connectDB();
 
+//To test the connection
+app.get('/', (req, res) => res.send('API Running'));
+
+//Define Routes
+app.use('/api/user', require('./routes/api/user'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'))
 
 const PORT  = process.env.PORT || 5000;
 
